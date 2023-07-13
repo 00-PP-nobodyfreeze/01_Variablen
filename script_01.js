14861f2447e35cffa188d7c0e674c4bac4fef87a
Peter <schmidt.multimedia@gmail.com>
2023-07-13 12:13:06 +0200

    update

--- script_01.js
+++ script_01.js
@@ -58,11 +58,13 @@ let ageJohn, ageMark;
 const birthYearJohn = 2000;
 const birthYearMark = 1990;
 
+// Berechnung
 let date = new Date(); // Systemfunktion Datum
 let year = date.getFullYear(); // Daten liefern lassen!
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
+// Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 
