a693e1a1254723824bbfc442ed9608415900edaf
nobodyfreeze <office.fherzog@gmail.com>
2023-07-13 11:46:17 +0200

    03a example

--- script_01.js
+++ script_01.js
@@ -15,10 +15,39 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName;              // Deklaration
-firstName = "Max";          // Wertzuweisung
-console.log(firstName);     // Ausgabe
+// let firstName;              // Deklaration
+// firstName = "Max";          // Wertzuweisung
+// console.log(firstName);     // Ausgabe
 
-let familyName;
-familyName = "Mütze";
-console.log(familyName);
+// let familyName;
+// familyName = "Mütze";
+// console.log(familyName);
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+// let firstName, familyName;
+
+// firstName = prompt("Bitte Vornamen eingeben");
+// familyName = prompt("Bitte Nachnamen eingeben");
+
+// console.log(firstName + " " + familyName);
+
+/* JS ist eine untypisierte Sprache! | untyped */
+// let test;
+// test = "Maxine";
+// // test = 2;
+// // test = false;
+
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+
+/***** 03a Variablen vs. Konstanten *******/
+
+// let firstName = "Max";      // Deklaration + Wertzuweisung
+// firstName ="Maxine";
+// console.log(firstName);     // Ausgabe
+
+// const firstName = "Max";       // Variable mit KONSTANTEM Inhalt
+// firstName ="Maxine";           // KEINE neue Zuweisung zur LZ möglich, gibt Fehlermeldung aus
+// console.log(firstName);        // Ausgabe
+
